3969f52dc297a75983453c2e1270ada814fdd05c
Lee Briggs <lee@leebriggs.co.uk>
2020-07-09 17:14:58 -0700

    scaffold initial vpc

---
+++ go/main.go
@@ -0,0 +1,36 @@
+package vpc
+
+import (
+	"fmt"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
+)
+
+type Vpc struct {
+	pulumi.ResourceState
+}
+
+type VpcArgs struct {
+	baseCidr pulumi.String
+}
+
+func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.ResourceOption) (*Vpc, error) {
+	vpc := &Vpc{}
+
+	_, err := ec2.NewVpc(ctx, fmt.Sprintf("%s-vpc", name), &ec2.VpcArgs{
+		CidrBlock: args.baseCidr,
+		EnableDnsSupport: pulumi.Bool(true),
+		EnableDnsHostnames: pulumi.Bool(true),
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	// Register component resource
+	err = ctx.RegisterComponentResource("jen20:aws-vpc", name, vpc, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return vpc, nil
+}
