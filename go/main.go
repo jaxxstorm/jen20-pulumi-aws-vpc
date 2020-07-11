ac6aa3a3ec71f515e42b0539b34ebc3e97710324
Lee Briggs <lee@leebriggs.co.uk>
2020-07-11 14:46:29 -0700

    add route53 private domain

--- go/main.go
+++ go/main.go
@@ -2,29 +2,32 @@ package vpc
 
 import (
 	"fmt"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/route53"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type Vpc struct {
 	pulumi.ResourceState
 
-	ID pulumi.IDOutput `pulumi:"ID"`
+	ID   pulumi.IDOutput     `pulumi:"ID"`
 	Cidr pulumi.StringOutput `pulumi:"Cidr"`
-	Arn pulumi.StringOutput `pulumi:"Arn"`
+	Arn  pulumi.StringOutput `pulumi:"Arn"`
 }
 
-type VpcArgs struct {
+type Args struct {
 	BaseCidr pulumi.String
+	ZoneName pulumi.String
 }
 
-func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.ResourceOption) (*Vpc, error) {
+func NewVpc(ctx *pulumi.Context, name string, args Args, opts ...pulumi.ResourceOption) (*Vpc, error) {
 	vpc := &Vpc{}
 
 	// create the VPC
 	awsVpc, err := ec2.NewVpc(ctx, fmt.Sprintf("%s-vpc", name), &ec2.VpcArgs{
-		CidrBlock: args.BaseCidr,
-		EnableDnsSupport: pulumi.Bool(true),
+		CidrBlock:          args.BaseCidr,
+		EnableDnsSupport:   pulumi.Bool(true),
 		EnableDnsHostnames: pulumi.Bool(true),
 	}, pulumi.Parent(vpc))
 	if err != nil {
@@ -41,6 +44,44 @@ func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.Resou
 		VpcId: awsVpc.ID(),
 	}, pulumi.Parent(awsVpc))
 
+	/*
+	 * if a zone name is specified, create a route53 zone
+	 * and set some DHCP options for the VPC
+	 */
+	if args.ZoneName != "" {
+		// creates the route53 private zone
+		zone, err := route53.NewZone(ctx, fmt.Sprintf("%s-private-zone", name), &route53.ZoneArgs{
+			Name:    args.ZoneName,
+			Comment: pulumi.Sprintf("Private zone for %s. Managed by Pulumi", args.ZoneName),
+			Vpcs: route53.ZoneVpcArray{
+				&route53.ZoneVpcArgs{
+					VpcId: awsVpc.ID(),
+				},
+			},
+		}, pulumi.Parent(vpc))
+		if err != nil {
+			return nil, err
+		}
+
+		// creates the DHCP option set
+		dhcpOptionSet, err := ec2.NewVpcDhcpOptions(ctx, fmt.Sprintf("%s-dhcp-options", name), &ec2.VpcDhcpOptionsArgs{
+			DomainName: zone.Name,
+			DomainNameServers: pulumi.StringArray{
+				pulumi.String("AmazonProvidedDNS"),
+			},
+		}, pulumi.Parent(awsVpc))
+		if err != nil {
+			return nil, err
+		}
+		_, err = ec2.NewVpcDhcpOptionsAssociation(ctx, fmt.Sprintf("%s-dhcp-options-assoc", name), &ec2.VpcDhcpOptionsAssociationArgs{
+			VpcId:         awsVpc.ID(),
+			DhcpOptionsId: dhcpOptionSet.ID(),
+		}, pulumi.Parent(dhcpOptionSet))
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	// Register component resource
 	err = ctx.RegisterComponentResource("jen20:aws-vpc", name, vpc, opts...)
 	if err != nil {
@@ -48,9 +89,9 @@ func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.Resou
 	}
 
 	ctx.RegisterResourceOutputs(vpc, pulumi.Map{
-		"ID": awsVpc.ID(),
+		"ID":   awsVpc.ID(),
 		"Cidr": awsVpc.CidrBlock,
-		"Arn": awsVpc.Arn,
+		"Arn":  awsVpc.Arn,
 	})
 
 	return vpc, nil
