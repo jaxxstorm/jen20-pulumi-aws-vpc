45542dfb30c92a032f0c521fb816a23289329e2f
Lee Briggs <lee@leebriggs.co.uk>
2020-07-11 14:27:22 -0700

    add an internet gateway

--- go/main.go
+++ go/main.go
@@ -21,6 +21,7 @@ type VpcArgs struct {
 func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.ResourceOption) (*Vpc, error) {
 	vpc := &Vpc{}
 
+	// create the VPC
 	awsVpc, err := ec2.NewVpc(ctx, fmt.Sprintf("%s-vpc", name), &ec2.VpcArgs{
 		CidrBlock: args.BaseCidr,
 		EnableDnsSupport: pulumi.Bool(true),
@@ -30,9 +31,15 @@ func NewVpc(ctx *pulumi.Context, name string, args VpcArgs, opts ...pulumi.Resou
 		return nil, err
 	}
 
+	// export some VPC outputs
 	vpc.ID = awsVpc.ID()
 	vpc.Cidr = awsVpc.CidrBlock
 	vpc.Arn = awsVpc.Arn
+
+	// add an internet gateway
+	_, err = ec2.NewInternetGateway(ctx, fmt.Sprintf("%s-igw", name), &ec2.InternetGatewayArgs{
+		VpcId: awsVpc.ID(),
+	}, pulumi.Parent(awsVpc))
 
 	// Register component resource
 	err = ctx.RegisterComponentResource("jen20:aws-vpc", name, vpc, opts...)
